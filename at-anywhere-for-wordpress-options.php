1f55b602e349633a6763c24edf618f083e714384
Robby Grossman <robby@freerobby.com>
2010-04-16 19:34:13 -0400

    Hovers now completely functional.

--- at-anywhere-for-wordpress-options.php
+++ at-anywhere-for-wordpress-options.php
@@ -5,17 +5,13 @@
     <?php wp_nonce_field('update-options'); ?>
     <table class="form-table">
       <tr valign="top">
-        <th scope="row">@anywhere Consumer Key</th>
-        <td><input type="text" name="aa_consumer_key" value="<?php echo get_option('aa_consumer_key'); ?>" /></td>
-      </tr>
-      <tr valign="top">
-        <th scope="row">@anywhere Consumer Secret</th>
-        <td><input type="text" name="aa_consumer_secret" value="<?php echo get_option('aa_consumer_secret'); ?>" /></td>
+        <th scope="row">@anywhere API Key</th>
+        <td><input type="text" name="aa_api_key" value="<?php echo get_option('aa_api_key'); ?>" size="30" /></td>
       </tr>
     </table>
 
     <input type="hidden" name="action" value="update" />
-    <input type="hidden" name="page_options" value="aa_consumer_key,aa_consumer_secret" />
+    <input type="hidden" name="page_options" value="aa_api_key" />
 
     <p class="submit">
     <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
--- at-anywhere-for-wordpress.php
+++ at-anywhere-for-wordpress.php
@@ -10,13 +10,27 @@ License:      MIT
 */
 
 // Create our plugin options page.
-
 add_action('admin_menu', 'my_plugin_menu');
 function my_plugin_menu() {
   add_options_page('Configure @anywhere for Wordpress', 'Configure @anywhere', 'administrator', 'atanywhereoptions', 'at_anywhere_options'); 
 }
 function at_anywhere_options() {
   require('at-anywhere-for-wordpress-options.php');
+}
+
+// Add @anywhere Javascript to page
+add_action( 'wp_head', 'generate_at_anywhere_scripts' );
+function generate_at_anywhere_scripts() {
+  $key = get_option('aa_api_key');
+  $my_str = "<script src=\"http://platform.twitter.com/anywhere.js?id=$key&v=1\">"
+    . "</script>"
+    . "<script type=\"text/javascript\">"
+    . "  twttr.anywhere(function(twitter) {"
+    . "    twitter.hovercards();"
+    . "  });"
+    . "</script>";
+  
+  echo $my_str;
 }
 
 ?>
