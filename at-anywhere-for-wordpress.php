d9c92ef7374cb8c8670e3d41b66830bc3b0ac09e
Robby Grossman <robby@freerobby.com>
2010-04-16 18:41:27 -0400

    Configure plugin header.

---
+++ at-anywhere-for-wordpress.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:  @anywhere for Wordpress
+Plugin URI:   http://freerobby.com/at-anywhere-for-wordpress
+Description:  Integrates Twitter's @anywhere service into your Wordpress blog
+Version:      1.0
+Author:       Robby Grossman
+Author URI:   http://freerobby.com
+License:      MIT
+*/
+?>
